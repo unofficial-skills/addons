563f72b40dfd62b798641bc11765522c6b843848
andrewstech <45342431+andrewstech@users.noreply.github.com>
2021-04-18 18:25:34 +0100

    Update run.sh

--- alpha-video/run.sh
+++ alpha-video/run.sh
@@ -13,7 +13,7 @@ echo "For support please visit the Github Project or send a message on our Disco
 echo "set nameserver to 1.1.1.1"
 echo nameserver 1.1.1.1 > /etc/resolv.conf
 echo "Starting Alpha-Video"
-python /app/main.py > /var/log/alpha-video.log&
+python3 /app/main.py > /var/log/alpha-video.log&
 echo "Starting Supervisord"
 /usr/bin/supervisord -n -c /etc/supervisor/supervisord.conf
 bash
