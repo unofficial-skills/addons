ae2aff056859698315c3fdeafa22d8880cd791b9
andrewstech <45342431+andrewstech@users.noreply.github.com>
2021-04-24 00:13:34 +0100

    Update run.sh

--- alpha-video/run.sh
+++ alpha-video/run.sh
@@ -2,12 +2,6 @@
 CONFIG_PATH=/data/options.json
 subdomain="$(jq --raw-output 'subdomain' $CONFIG_PATH)" 
 export subdomain
-echo "          _      _____  _    _           __      _______ _____  ______ ____  "
-echo "    /\   | |    |  __ \| |  | |   /\     \ \    / /_   _|  __ \|  ____/ __ \ "
-echo "   /  \  | |    | |__) | |__| |  /  \     \ \  / /  | | | |  | | |__ | |  | |"
-echo "  / /\ \ | |    |  ___/|  __  | / /\ \     \ \/ /   | | | |  | |  __|| |  | |"
-echo " / ____ \| |____| |    | |  | |/ ____ \     \  /   _| |_| |__| | |___| |__| |"
-echo "/_/    \_\______|_|    |_|  |_/_/    \_\     \/   |_____|_____/|______\____/ "
 
 echo "For support please visit the Github Project or send a message on our Discord server."
 echo "set nameserver to 1.1.1.1"
