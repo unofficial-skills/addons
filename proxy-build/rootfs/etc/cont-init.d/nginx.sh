8aa6c48c75fa7c05329fbf0c22a643072b4d3681
andrewstech <45342431+andrewstech@users.noreply.github.com>
2021-05-19 10:48:35 +0100

    b
    bruhh

---
+++ proxy-build/rootfs/etc/cont-init.d/nginx.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/with-contenv bashio
+# ==============================================================================
+# Home Assistant Community Add-on: NGINX Proxy Manager
+# Configures NGINX for use with the NGINX Proxy Manager
+# ==============================================================================
+declare dns_host
+
+dns_host=$(bashio::dns.host)
+sed -i "s/%%dns_host%%/${dns_host}/g" \
+    /etc/nginx/conf.d/include/resolvers.conf
