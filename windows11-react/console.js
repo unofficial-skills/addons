fc984e42bad43c5163f97473b3c7dc75d59db434
andrewstech <info@andrewstech.me>
2021-10-28 15:27:47 +0100

    bruhh

--- windows11-react/console.js
+++
@@ -1,5 +0,0 @@
-console.log("Windows 11 in React is a project by blueedgetechno. If you wish to help out with this project visit https://github.com/blueedgetechno/windows11");
-
-function removeWarning() {
-  document.body.dataset.force = true;
-}
